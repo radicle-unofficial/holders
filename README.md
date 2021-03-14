20383ee9459928ce71dcd0a962bec173c9a5be22
clearloop <udtrokia@gmail.com>
2021-03-15 03:02:13 +0800

    feat(txns): getting all transfer events from the LBP

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # LBP Holders
 
-This repo if for counting the holders of RAD from LBP till now.
+This repo is for counting the holders of RAD from LBP till now.
+
+Here we are using ethplorer for fetching the transfer records, 
+for using this script yourselves for checking the result, go to 
+https://ethplorer.io/ and generate a API_KEY incase fetching
+the txns with limit 10.
+
 
 ## LICENSE
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Date     Feb-25-2021 04:39:03 PM +UTC
 FromTX   0xade852fd2265723b66198b46dd08718e1754fd0b0468ad1d25651355ef9984db
 '''
 import os
+import sys
 import calendar
 import time
 import json
@@ -26,20 +27,26 @@ def key():
     if os.path.exists(API_KEY_PATH) is False:
         return 'freekey'
 
-    with open("API_KEY") as api_key:
+    with open(API_KEY_PATH) as api_key:
         return api_key.read()
 
 
 def txns(api_key, timestamp, history):
     '''
-    Get all txns of Radicle
+    Get all transfer events of Radicle from the LBP
     '''
     print(f'Fetching txns start at {timestamp}...')
-    res = r.get(TXNS.replace("{TS}", str(timestamp)).replace('{KEY}', api_key))
-    ops = json.loads(res.text)['operations']
+    res = json.loads(r.get(
+        TXNS.replace("{TS}", str(timestamp)).replace('{KEY}', api_key)
+    ).text)
+
+    # If request failed
+    if 'error' in res:
+        print(res['error']['message'])
+        sys.exit(1)
 
     # Apply new format for counting
-    for operation in ops:
+    for operation in res['operations']:
         # Finish fetching while reaching the initial tx of LBP
         if operation['transactionHash'] == LBP:
             return history
@@ -54,7 +61,7 @@ def txns(api_key, timestamp, history):
 
     # Keep fetching to the LBP init TX
     print(f'Currentt txns: {len(history)}')
-    return txns(key, history[len(history) - 1]['timestamp'], history)
+    return txns(api_key, history[len(history) - 1]['timestamp'], history)
 
 
 def main():
@@ -62,7 +69,7 @@ def main():
     Main FN
     '''
     timestamp = calendar.timegm(time.gmtime())
-    with open('holders.json') as out:
+    with open('holders.json', 'w') as out:
         json.dump(txns(key(), timestamp, []), out)
 
 
